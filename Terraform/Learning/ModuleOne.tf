5f0cf4865676ab748f791c28f6a7a2668b404b14
Dominic Clayton <dominic.clayton@zeninternet.co.uk>
2019-10-03 17:14:32 +0100

    Fixes for auth using service principal.

--- Terraform/Learning/ModuleOne.tf
+++ Terraform/Learning/ModuleOne.tf
@@ -1,9 +1,9 @@
 ########################
 # Variables
 ########################
+variable "ARM_SUBSCRIPTION_ID" {}
 variable "ARM_CLIENT_ID" {}
 variable "ARM_CLIENT_SECRET" {}
-variable "ARM_SUBSCRIPTION_ID" {}
 variable "ARM_TENANT_ID" {}
 
 ########################
@@ -12,5 +12,16 @@ variable "ARM_TENANT_ID" {}
 
 provider "azurerm" {
     version = "~> 1.34"
+
+    subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
+    client_id = "${var.ARM_CLIENT_ID}"
+    client_secret = "${var.ARM_CLIENT_SECRET}"
+    tenant_id = "${var.ARM_TENANT_ID}"
+
 }
+resource "azurerm_resource_group" "myRg" {
+    name = "myRG"
+    location = "northeurope"
+}
+
 

a188092751b937317419de25215f06c7c73e2560
Dominic Clayton <dominic.clayton@zeninternet.co.uk>
2019-10-03 16:35:18 +0100

    First attempt at Terraform

---
+++ Terraform/Learning/ModuleOne.tf
@@ -0,0 +1,16 @@
+########################
+# Variables
+########################
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_TENANT_ID" {}
+
+########################
+# Provider
+########################
+
+provider "azurerm" {
+    version = "~> 1.34"
+}
+

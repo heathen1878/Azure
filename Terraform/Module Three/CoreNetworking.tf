0a08db0ad943f9fcdda19da7e48e6a746348d51e
Dominic Clayton <dominic.clayton@zeninternet.co.uk>
2019-10-07 11:57:48 +0100

    Removed provider and added networking configuration resources.

--- Terraform/Module Three/CoreNetworking.tf
+++ Terraform/Module Three/CoreNetworking.tf
@@ -1,19 +1,64 @@
-# Providers
-# Azure RM
-provider "azurerm" {
-      # Locked to version
-    version = "~> 1.34"
-
-    # consume variables from terraform.tfvars
-    subscription_id = "${var.My_Subscription_Id}"
-    client_id = "${var.My_Client_Id}"
-    client_secret = "${var.My_Client_Secret}"
-    tenant_id = "${var.My_Tenant_Id}"
-}
-
 # Resources
 resource "azurerm_resource_group" "CoreNetworkingRG" {
     name = "DJC-NE-TRAIN-IT-NET-INT-RG"
     location = "${var.Location}"
-    tags = "${var.NSGTags}"
+    tags = "${var.CoreNetworkingTags}"
 }
+
+resource "azurerm_public_ip" "publicIPAddress" {
+    name = "vpnGatewayPublicIPAddress"
+    resource_group_name = "${azurerm_resource_group.CoreNetworkingRG.name}"
+    location = "${azurerm_resource_group.CoreNetworkingRG.location}"
+    tags = "${azurerm_resource_group.CoreNetworkingRG.tags}"
+    allocation_method = "Dynamic"   
+}
+
+resource "azurerm_virtual_network" "CoreVNet" {
+    name = "Core"
+    resource_group_name = "${azurerm_resource_group.CoreNetworkingRG.name}"
+    location = "${azurerm_resource_group.CoreNetworkingRG.location}"
+    tags = "${azurerm_resource_group.CoreNetworkingRG.tags}"
+    address_space = ["10.0.0.0/16"]
+    dns_servers = ["8.8.8.8","4.4.4.4"]
+}
+
+resource "azurerm_subnet" "Gateway" {
+    name = "Gateway"
+    resource_group_name = "${azurerm_resource_group.CoreNetworkingRG.name}"
+    virtual_network_name = "${azurerm_virtual_network.CoreVNet.name}"
+    address_prefix = "10.0.0.0/24"
+}
+
+resource "azurerm_subnet" "Training" {
+    name = "Training"
+    resource_group_name = "${azurerm_resource_group.CoreNetworkingRG.name}"
+    virtual_network_name = "${azurerm_virtual_network.CoreVNet.name}"
+    address_prefix = "10.0.1.0/24"
+}
+
+resource "azurerm_subnet" "Development" {
+    name = "Development"
+    resource_group_name = "${azurerm_resource_group.CoreNetworkingRG.name}"
+    virtual_network_name = "${azurerm_virtual_network.CoreVNet.name}"
+    address_prefix = "10.0.2.0/24"
+}
+    
+resource "azurerm_virtual_network_gateway" "CoreVNetGateway" {
+    name = "CoreVNetGateway"
+    resource_group_name = "${azurerm_resource_group.CoreNetworkingRG.name}"
+    location = "${azurerm_resource_group.CoreNetworkingRG.location}"
+    tags = "${azurerm_resource_group.CoreNetworkingRG.tags}"
+    type = "vpn"
+    vpn_type = "RouteBased"
+    sku = "Basic"
+    enable_bgp = true
+
+    ip_configuration {
+        name = "CoreVNetGatewaySubnet"
+        public_ip_address_id = "${azurerm_public_ip.publicIPAddress.id}"
+        private_ip_address_allocation = "Dynamic"
+        subnet_id = "${azurerm_subnet.Gateway.id}"
+    }
+}
+
+

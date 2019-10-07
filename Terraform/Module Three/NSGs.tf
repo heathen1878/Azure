c3289cf80d1bb5d43d41e174f8f09d14243c7fde
Dominic Clayton <dominic.clayton@zeninternet.co.uk>
2019-10-07 11:23:54 +0100

    Added missing variables and fixed tag variable. Added resource provider and fixed typos.

--- Terraform/Module Three/NSGs.tf
+++ Terraform/Module Three/NSGs.tf
@@ -1,3 +1,17 @@
+# Providers
+# Azure RM
+provider "azurerm" {
+      # Locked to version
+    version = "~> 1.34"
+
+    # consume variables from terraform.tfvars
+    subscription_id = "${var.My_Subscription_Id}"
+    client_id = "${var.My_Client_Id}"
+    client_secret = "${var.My_Client_Secret}"
+    tenant_id = "${var.My_Tenant_Id}"
+}
+
+# Resources
 resource "azurerm_resource_group" "NSGs" {
     name = "DJC-NE-TRAIN-IT-NSGs-INT-RG"
     location = "${var.Location}"
@@ -5,14 +19,14 @@ resource "azurerm_resource_group" "NSGs" {
 }
 
 resource "azurerm_network_security_group" "DefaultNSGs" {
-    name = "Default NSGs"
-    resource_group_name = "${azurerm_network_security_group.DefaultNSGs.name}"
-    location = "${azurerm_network_security_group.DefaultNSGs.location}"
-    tags = "${azurerm_network_security_group.DefaultNSGs.tags}"
+    name = "DefaultNSG"
+    resource_group_name = "${azurerm_resource_group.NSGs.name}"
+    location = "${azurerm_resource_group.NSGs.location}"
+    tags = "${azurerm_resource_group.NSGs.tags}"
 }
 
 resource "azurerm_network_security_rule" "AllowHTTP" {
-    name = "Allow HTTP"
+    name = "AllowHTTP"
     resource_group_name = "${azurerm_resource_group.NSGs.name}"
     network_security_group_name = "${azurerm_network_security_group.DefaultNSGs.name}"
 
@@ -26,3 +40,64 @@ resource "azurerm_network_security_rule" "AllowHTTP" {
     source_address_prefix = "*"
 }
 
+resource "azurerm_network_security_rule" "AllowHTTPS" {
+    name = "AllowHTTPS"
+    resource_group_name = "${azurerm_resource_group.NSGs.name}"
+    network_security_group_name = "${azurerm_network_security_group.DefaultNSGs.name}"
+
+    priority = 1011
+    access = "Allow"
+    direction = "Inbound"
+    protocol = "Tcp"
+    destination_port_range = 443
+    destination_address_prefix = "*"
+    source_port_range = "*"
+    source_address_prefix = "*"
+}
+resource "azurerm_network_security_rule" "AllowMSSQL" {
+    name = "AllowMSSQL"
+    resource_group_name = "${azurerm_resource_group.NSGs.name}"
+    network_security_group_name = "${azurerm_network_security_group.DefaultNSGs.name}"
+
+    priority = 1012
+    access = "Allow"
+    direction = "Inbound"
+    protocol = "Tcp"
+    destination_port_range = 1443
+    destination_address_prefix = "*"
+    source_port_range = "*"
+    source_address_prefix = "*"
+}
+resource "azurerm_network_security_rule" "AllowRDP" {
+    name = "AllowRDP"
+    resource_group_name = "${azurerm_resource_group.NSGs.name}"
+    network_security_group_name = "${azurerm_network_security_group.DefaultNSGs.name}"
+
+    priority = 1013
+    access = "Allow"
+    direction = "Inbound"
+    protocol = "Tcp"
+    destination_port_range = 3389
+    destination_address_prefix = "*"
+    source_port_range = "*"
+    source_address_prefix = "*"
+}
+
+resource "azurerm_network_security_group" "WindowsNicNsg" {
+    name = "ApplyToWindowsServers"
+    resource_group_name = "${azurerm_resource_group.NSGs.name}"
+    location = "${azurerm_resource_group.NSGs.location}"
+    tags = "${azurerm_resource_group.NSGs.tags}"
+
+    security_rule {
+       name = "AllowRDP"
+       priority = 100
+        access = "Allow"
+        direction = "Inbound"
+        protocol = "Tcp"
+        destination_port_range = 3389
+        destination_address_prefix = "*"
+        source_port_range = "*"
+        source_address_prefix = "*"
+    }
+}
--- Terraform/Module Three/variables.tf
+++ Terraform/Module Three/variables.tf
@@ -1,30 +1,47 @@
 # Variables
+variable "My_Subscription_Id" {
+  
+}
+variable "My_Client_Id" {
+  
+}
+variable "My_Client_Secret" {
+  
+}
+variable "My_Tenant_Id" {
+}
 variable "CoreNetworkingTags" {
-    Environment = "Training" 
-    MaintenanceWindows = ""
-    ExpirationDate = ""
-    TimeWindow = ""
-    Department = ""
-    ApplicationName = "Networking"
-    CostCenter = "IT"
-    Description = "Resource group for core networking"
-    TechnicalContact = "Cloud Engineering"
-    DataClassification = "Internal Use Only"
-    RegulatoryCompliance = ""
+    type = "map"
+    default = {
+        Environment = "Training" 
+        MaintenanceWindows = ""
+        ExpirationDate = ""
+        TimeWindow = ""
+        Department = ""
+        ApplicationName = "Networking"
+        CostCenter = "IT"
+        Description = "Resource group for core networking"
+        TechnicalContact = "Cloud Engineering"
+        DataClassification = "Internal Use Only"
+        RegulatoryCompliance = ""
+    }
 }
 
 variable "NSGTags" {
-    Environment = "Training" 
-    MaintenanceWindows = ""
-    ExpirationDate = ""
-    TimeWindow = ""
-    Department = ""
-    ApplicationName = "Security"
-    CostCenter = "IT"
-    Description = "Resource group for security configuration"
-    TechnicalContact = "Cloud Engineering"
-    DataClassification = "Internal Use Only"
-    RegulatoryCompliance = ""
+    type = "map"
+    default = {
+        Environment = "Training" 
+        MaintenanceWindows = ""
+        ExpirationDate = ""
+        TimeWindow = ""
+        Department = ""
+        ApplicationName = "Security"
+        CostCenter = "IT"
+        Description = "Resource group for security configuration"
+        TechnicalContact = "Cloud Engineering"
+        DataClassification = "Internal Use Only"
+        RegulatoryCompliance = ""
+    }
 }
 
 variable "Location" {

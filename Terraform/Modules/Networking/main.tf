4d332085a4ed89697066366d0a03ecfcf1702f76
Dominic Clayton <dominic.clayton@zeninternet.co.uk>
2019-10-11 14:56:21 +0100

    Defined policies, and locks for network resources

--- Terraform/Modules/Networking/main.tf
+++ Terraform/Modules/Networking/main.tf
@@ -1,14 +1,22 @@
+/* Resource Group for networking */
 resource "azurerm_resource_group" "NetworkRG" {
     name = "${upper(var.CompanyNamePrefix)}-${upper(var.NetworkRGLocation)}-${upper(var.environment)}-NETWORK-RG"
     location = "${var.NetworkRGLocation}"
     tags = "${var.NetworkRGTags}"
 }
 
+/* GOVERNANCE */
+/* 
+Policy for checking tagging compliance
+*/
 data "azurerm_policy_definition" "RequireTagsOnRG" {
     display_name = "Require tag and its value on resource groups"
     depends_on = ["azurerm_resource_group.NetworkRG"]
 }
 
+/* 
+Policy assignment for checking tagging compliance
+*/
 resource "azurerm_policy_assignment" "MandatoryTagPolicy" {
     for_each = "${var.NetworkRGTags}"
     name = "Require ${each.key} and its value on resource groups"
@@ -31,6 +39,81 @@ resource "azurerm_policy_assignment" "MandatoryTagPolicy" {
     depends_on = ["azurerm_resource_group.NetworkRG"]
 }
 
+/*
+Policy assignment for defining allowed resources
+*/
+data "azurerm_policy_definition" "AllowedResourceTypes" {
+    display_name = "Allowed resource types"
+    depends_on = ["azurerm_resource_group.NetworkRG"]
+}
+
+resource "azurerm_policy_assignment" "AllowedResources" {
+    name = "Resource types allowed in ${azurerm_resource_group.NetworkRG.name}"
+    scope = "${azurerm_resource_group.NetworkRG.id}"
+    policy_definition_id = "${data.azurerm_policy_definition.AllowedResourceTypes.id}"
+    description = "Defines what resource types can be created within the resource group: ${azurerm_resource_group.NetworkRG.name}"
+    display_name = "Resource types allowed in ${azurerm_resource_group.NetworkRG.name}"
+
+    parameters = <<PARAMETERS
+    {
+        "listOfResourceTypesAllowed": {
+            "value": [ "Microsoft.Authorization/locks","Microsoft.Authorization/policyAssignments","Microsoft.Authorization/roleAssignments","Microsoft.Network/networkInterfaces","Microsoft.Network/networkSecurityGroups","Microsoft.Network/networkSecurityGroups/securityRules","Microsoft.Network/azureFirewalls","Microsoft.Network/loadBalancers","Microsoft.Network/vpnGateways","Microsoft.Network/virtualNetworks","Microsoft.Network/virtualNetworks/subnets","Microsoft.Network/publicIPAddresses","Microsoft.Network/virtualNetworkGateways","Microsoft.Resource/checkPolicyCompliance" ]
+        }
+    }
+    PARAMETERS
+
+    depends_on = ["azurerm_resource_group.NetworkRG"]
+}
+
+/*
+Policy assignment for defining allowed locations
+*/
+data "azurerm_policy_definition" "AllowedLocations" {
+    display_name = "Allowed locations"
+       depends_on = ["azurerm_resource_group.NetworkRG"]
+}
+
+resource "azurerm_policy_assignment" "AllowedLocations" {
+    name = "Resources can be deployed in ${var.NetworkRGLocation}"
+    scope = "${azurerm_resource_group.NetworkRG.id}"
+    policy_definition_id = "${data.azurerm_policy_definition.AllowedLocations.id}"
+    description = "Defines the location: ${var.NetworkRGLocation} resource can be deployed in."
+    display_name = "Resources can be deployed in ${var.NetworkRGLocation}"
+
+    parameters = <<PARAMETERS
+    {
+        "listOfAllowedLocations": {
+            "value": ["${var.NetworkRGLocation}"]
+        }
+    }
+    PARAMETERS
+
+    depends_on = ["azurerm_resource_group.NetworkRG"]
+}
+
+/* RBAC */
+/*
+RBAC is applied at the subscription level. 
+*/
+
+
+/* Management Locks */
+/*
+Should be the last configuration applied.
+*/
+resource "azurerm_management_lock" "read_only" {
+    name = "${azurerm_resource_group.NetworkRG.name}-readonly"
+    scope = "${azurerm_resource_group.NetworkRG.id}"
+    lock_level = "ReadOnly"
+    notes = "${azurerm_resource_group.NetworkRG.name} is read only"
+
+    depends_on = ["azurerm_virtual_network_gateway.VNetGateway"]
+}
+/*
+END OF GOVERNANCE
+*/
+/* RESOURCES */
+/* Create a virtual network */
 resource "azurerm_virtual_network" "VNet" {
     name = "${upper(var.CompanyNamePrefix)}-${upper(var.NetworkRGLocation)}-${upper(var.environment)}-VNET-${replace(upper(var.VNetAddressSpace[0]),"/","-")}"
     resource_group_name = "${azurerm_resource_group.NetworkRG.name}"
@@ -39,6 +122,7 @@ resource "azurerm_virtual_network" "VNet" {
     dns_servers = "${var.VNetDNSServers}"
 }
 
+/* Create a public IP address for the VPN gateway */
 resource "azurerm_public_ip" "publicIPAddress" {
     name = "${azurerm_virtual_network.VNet.name}-PIP-1"
     resource_group_name = "${azurerm_resource_group.NetworkRG.name}"
@@ -46,9 +130,10 @@ resource "azurerm_public_ip" "publicIPAddress" {
     allocation_method = "Dynamic"   
 }
 
+/* Create a number of subnets as per the address prefix variable */
 resource "azurerm_subnet" "Subnet" {
     for_each = "${var.AddressPrefix}"
-    name = <<-EOT
+    name =<<-EOT
 %{~ if "${each.key}" != "gateway" ~}
 ${upper(var.CompanyNamePrefix)}-${upper(var.NetworkRGLocation)}-${upper(var.environment)}-${upper(each.key)}-${replace(upper(each.value),"/","-")}
 %{~ else ~}
@@ -59,7 +144,8 @@ EOT
     virtual_network_name = "${azurerm_virtual_network.VNet.name}"
     address_prefix = "${each.value}"
 }
-     
+
+/* Get the Gateway Subnet id */     
 data "azurerm_subnet" "gateway" {
     name = "GatewaySubnet"
     virtual_network_name = "${upper(var.CompanyNamePrefix)}-${upper(var.NetworkRGLocation)}-${upper(var.environment)}-VNET-${replace(upper(var.VNetAddressSpace[0]),"/","-")}"
@@ -67,6 +153,7 @@ data "azurerm_subnet" "gateway" {
     depends_on = ["azurerm_subnet.Subnet"]
 } 
 
+/* Create a VPN gateway and assign the public IP address and gateway subnet previously created */
 resource "azurerm_virtual_network_gateway" "VNetGateway" {
     name = "${upper(var.CompanyNamePrefix)}-${upper(var.NetworkRGLocation)}-${upper(var.environment)}-${replace(upper(var.VNetAddressSpace[0]),"/","-")}-GW"
     resource_group_name = "${azurerm_resource_group.NetworkRG.name}"

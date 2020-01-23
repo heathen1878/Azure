3effbd4b5de887ffa20d36a050576c6871ba2e96
Dom Clayton <dominic@booandjoel.co.uk>
2020-01-23 16:48:53 +0000

    Updated PowerShell script to create resource groups

--- ARM/1.0-Landing-Zone/1.0-Landing-Zone-Resource-Groups.ps1
+++ ARM/1.0-Landing-Zone/1.0-Landing-Zone-Resource-Groups.ps1
@@ -35,7 +35,7 @@ Function New-ResourceGroup {
     }
 
     # Validate the location variable
-    $AzureLocations = Get-AzureRmLocation
+    $AzureLocations = Get-AzLocation
     if ($resourceGroupLocation -in ($AzureLocations).Location -or $resourceGroupLocation -in ($AzureLocations).DisplayName){
 
         Write-verbose "$resourceGroupLocation is valid" -verbose
@@ -46,6 +46,38 @@ Function New-ResourceGroup {
     }
     
     # Create a resource group from the variables above.
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Tag $resourceGroupTags
+    New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Tag $resourceGroupTags
 }
+
+<#
+Import resource group configurations
+#>
+$Config = @{
+    Configuration = '.\1.0-Landing-Zone-Resource-Groups.json'
+}
+
+# Import scheduling configuration from the external json file
+$ResourceGroups = Get-Content -Path $config.Configuration -Raw | ConvertFrom-Json
+
+<#
+Connect to Azure RM
+#>
+Connect-AzAccount
+<#
+Loop through each resource group definition and create it in the Azure subscription.
+#>
+$ResourceGroups.ResourceGroups | ForEach-Object {
+
+    New-ResourceGroup -applicationName $_.applicationNameTag `
+    -costCenter $_.costCenterTag `
+    -department $_.departmentTag `
+    -description $_.descriptionTag `
+    -technicalContact $_.technicalContactTag `
+    -resourceGroupName (-join($ResourceGroups.Company_Prefix.ToUpper(),"-",(-join($ResourceGroups.Location.Split(" ")).ToUpper()),"-",$ResourceGroups.Environment.ToUpper(),"-",$_.Name.ToUpper(),"-RG")) `
+    -resourceGroupLocation $ResourceGroups.Location
+
+}
+
+Disconnect-AzAccount
+
 

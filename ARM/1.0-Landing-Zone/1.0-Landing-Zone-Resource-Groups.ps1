c9fb7866979222b73280d1edf5e4e68bf1a307a9
Dom Clayton <dominic@booandjoel.co.uk>
2020-01-27 10:50:39 +0000

    Updated script information

--- ARM/1.0-Landing-Zone/1.0-Landing-Zone-Resource-Groups.ps1
+++ ARM/1.0-Landing-Zone/1.0-Landing-Zone-Resource-Groups.ps1
@@ -1,3 +1,32 @@
+<#
+    .SYNOPSIS
+        This script is used to create resource groups in Azure. The script takes input from a configuration file passed on the command line. 
+
+    .DESCRIPTION
+        Creates a number of resource groups in Azure based on the parameter file: '1.0-Landing-Zone-Resource-Groups.json'.
+
+    .PARAMETER PARAMETERFILE
+        This script requires a file name and path e.g. 
+        .\1.0-Landing-Zone-Resource-Groups.ps1 -ParameterFile ..\..\CustomerData\1.0-Landing-Zone\1.0-Landing-Zone-Resource-Groups.json
+
+    .NOTES
+        Version:        1.0.0.0
+        Author:         Dom Clayton
+        Creation Date:  24/01/2020 
+
+    .EXAMPLE
+        Set-ExecutionPolicy Bypass -Scope Process -Force;.\1.0-Landing-Zone-Resource-Groups.ps1 `
+        -ParameterFile ..\..\CustomerData\1.0-Landing-Zone\1.0-Landing-Zone-Resource-Groups.json
+      
+#>
+<#
+Parameter definition for the parameter file
+#>
+Param(
+    [Parameter(Mandatory=$true,Position=1)]
+    [string]$ParameterFile
+)
+
 Function New-ResourceGroup {
 
     [CmdletBinding()]
@@ -45,15 +74,23 @@ Function New-ResourceGroup {
         Break
     }
     
-    # Create a resource group from the variables above.
-    New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Tag $resourceGroupTags
+    # Create a resource group from the variables above, if it doesn't already exist.
+    If (!(Get-AzResourceGroup -Name $resourceGroupName)){
+
+        New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Tag $resourceGroupTags
+
+    } Else {
+
+        Write-Warning "$resourceGroupName already exists"
+
+    }
 }
 
 <#
 Import resource group configurations
 #>
 $Config = @{
-    Configuration = '.\1.0-Landing-Zone-Resource-Groups.json'
+    Configuration = $ParameterFile
 }
 
 # Import scheduling configuration from the external json file
@@ -62,22 +99,22 @@ $ResourceGroups = Get-Content -Path $config.Configuration -Raw | ConvertFrom-Jso
 <#
 Connect to Azure RM
 #>
-Connect-AzAccount
+Connect-AzAccount | Out-Null
 <#
 Loop through each resource group definition and create it in the Azure subscription.
 #>
 $ResourceGroups.ResourceGroups | ForEach-Object {
 
-    New-ResourceGroup -applicationName $_.applicationNameTag `
-    -costCenter $_.costCenterTag `
-    -department $_.departmentTag `
-    -description $_.descriptionTag `
-    -technicalContact $_.technicalContactTag `
-    -resourceGroupName (-join($ResourceGroups.Company_Prefix.ToUpper(),"-",(-join($ResourceGroups.Location.Split(" ")).ToUpper()),"-",$ResourceGroups.Environment.ToUpper(),"-",$_.Name.ToUpper(),"-RG")) `
-    -resourceGroupLocation $ResourceGroups.Location
+        New-ResourceGroup -applicationName $_.applicationNameTag `
+        -costCenter $_.costCenterTag `
+        -department $_.departmentTag `
+        -description $_.descriptionTag `
+        -technicalContact $_.technicalContactTag `
+        -resourceGroupName (-join($ResourceGroups.Company_Prefix.ToUpper(),"-",(-join($ResourceGroups.Location.Split(" ")).ToUpper()),"-",$ResourceGroups.Environment.ToUpper(),"-",$_.Name.ToUpper(),"-RG")) `
+        -resourceGroupLocation $ResourceGroups.Location
 
 }
 
-Disconnect-AzAccount
+Disconnect-AzAccount | Out-Null
 
 
